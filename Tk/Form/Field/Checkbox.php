197c73f8653f05dbc86488edfbc5630ce38d7644
tropotek <info@tropotek.com>
2017-02-28 13:56:43 +1100

    Finished moveing LDAP to a plugin, wahoo!

--- Tk/Form/Field/Checkbox.php
+++ Tk/Form/Field/Checkbox.php
@@ -3,6 +3,9 @@ namespace Tk\Form\Field;
 
 /**
  *
+ *
+ * @bug: little bug when fields are disabled, the checkbox state is assumed false instead of ignored? Not sure the fix here.
+ *
  * @author Michael Mifsud <info@tropotek.com>
  * @link http://www.tropotek.com/
  * @license Copyright 2015 Michael Mifsud
