49c7f46b593c53906ed814dc63651a7cb535d714
godar <info@tropotek.com>
2018-05-30 15:13:13 +1000

    ~Auto: Minor Code Updates - dev.ttek.org

--- Tk/Form.php
+++ Tk/Form.php
@@ -325,7 +325,8 @@ class Form extends Form\Element
         $fieldName = str_replace('[]', '', $fieldName);
         $field = $this->getField($fieldName);
         if (!$field || !$field instanceof Event\Iface) {
-            throw new Form\Exception('Event Field not found: `' . $fieldName . '`');
+            \Tk\Log::warning('Event Field not found: `' . $fieldName . '`');
+            //throw new Form\Exception('Event Field not found: `' . $fieldName . '`');
         }
         $field->addCallback($callback);
         return $field;
