211675db7cc64cfe1758f14ea8fc65efa6788b35
mifsudm <mifsudm@unimelb.edu.au>
2018-07-18 10:01:33 +1000

    Upgraded date fields to use datetimepicker plugin

--- Tk/Form/Renderer/FieldGroup.php
+++ Tk/Form/Renderer/FieldGroup.php
@@ -109,7 +109,7 @@ class FieldGroup extends \Dom\Renderer\Renderer implements \Dom\Renderer\Display
     {
         $xhtml = <<<HTML
 <div class="form-group form-group-sm" var="field-group">
-  <span class="help-block error-block" choice="errorText"><span class="" var="errorText"></span></span>
+  <span class="help-block error-block"><span class="" var="errorText" choice="errorText"></span></span>
   <label class="control-label" var="label" choice="label">
   </label><div var="element" class="controls"></div>
   <span class="help-block help-text" var="notes" choice="notes">&nbsp;</span>
