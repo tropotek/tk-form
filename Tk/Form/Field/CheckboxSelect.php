26a1622dfe26a431403bcef56077a35137ceca2a
mifsudm <mifsudm@unimelb.edu.au>
2018-12-19 13:27:34 +1100

    Added search to form Checkbox select

--- Tk/Form/Field/CheckboxSelect.php
+++ Tk/Form/Field/CheckboxSelect.php
@@ -10,6 +10,16 @@ namespace Tk\Form\Field;
 class CheckboxSelect extends CheckboxGroup
 {
 
+    protected $settings = array(
+        'search' => true,
+        'selectAll' => true
+    );
+
+
+
+
+
+
     /**
      * Get the element HTML
      *
@@ -64,7 +74,12 @@ class CheckboxSelect extends CheckboxGroup
             if ($this->getAttr('placeholder'))
                 $label = $this->getAttr('placeholder');
         }
-        $template->insertText('button', $label);
+        $template->insertText('value', $label);
+
+        if ($this->settings['search'])
+            $template->show('search');
+        if ($this->settings['selectAll'])
+            $template->show('select-all');
 
         $place = $this->getLabel();
         if ($this->getAttr('placeholder'))
@@ -81,27 +96,66 @@ jQuery(function (e) {
     form.find('.checkbox-select').each(function () {
       var select = $(this);
       var btn = select.find('.dropdown-toggle');
+      var searchInput = select.find('.input-search');
+      var clearBtn = select.find('.btn-clear');
+      var selectAll = select.find('.select-all-checkbox');
       
-      function updateButton() {
-        var label = [];
-        select.find(':checked').each(function () {
-          label.push($(this).closest('label').find('span').text());
-        });
-        if (label.length) {
-          btn.text(label.join(', '));
-        } else {
-          btn.text(btn.data('placeholder'));
-        }
-      }
+      // This is to add the value to the label, but no good in situations with large data-sets
+      // function updateButton() {
+      //   var label = [];
+      //   select.find(':checked').each(function () {
+      //     label.push($(this).closest('label').find('span').text());
+      //   });
+      //   if (label.length) {
+      //     btn.text(label.join(', '));
+      //   } else {
+      //     btn.text(btn.data('placeholder'));
+      //   }
+      // }
+      // $(this).find('.dropdown-menu input').on('click', function(e) {
+      //   //updateButton();
+      // });
       
       $(this).find('.dropdown-menu').on('click', function(e) {
         e.stopPropagation();
       });
-      $(this).find('.dropdown-menu input').on('click', function(e) {
-        //updateButton();
+      
+      clearBtn.on('click', function () {
+        searchInput.val('');
+        searchInput.trigger('keyup');
       });
+      
+      searchInput.on('keyup', function () {
+        var terms = $(this).val();
+        var list = select.find('.checkbox');
+        if (terms === '') {
+          list.show();
+          return;
+        }
+        list.hide();
+        list.filter(function () {
+            return $(this).text().toLowerCase().indexOf(terms.toLowerCase()) >= 0;
+        }).show();
+      });
+      
+      selectAll.on('change', function () {
+        if ($(this).prop('checked')) {
+          select.find('.checkbox input').prop('checked', true);
+        } else {
+          select.find('.checkbox input').prop('checked', false);
+        }
+      });
+      select.find('.checkbox input').on('change', function () {
+        if (select.find('.checkbox input').length !== select.find('.checkbox input:checked').length) {
+          selectAll.prop('checked', false);
+        } else {
+          selectAll.prop('checked', true);
+        }
+      });
+      
+      
+      
     });
-    
   }
   $('form').on('init', document, init).each(init);
 });
@@ -124,9 +178,20 @@ JS;
         $xhtml = <<<HTML
 <div class="dropdown checkbox-select" var="group">
   <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" var="button">
-    <span var="value">-- Select --</span>
+    <span><span var="value">-- Select --</span>  <i class="fa fa-caret-down"></i></span>
   </button>
   <ul class="dropdown-menu" aria-labelledby="dropdownMenuOffset" var="dropdown-menu">
+      
+      <li class="dropdown-item search" style="position: relative;" choice="search">
+        <input type="text" class="input-search" style="width: 100%;" placeholder="Search..."/>
+        <a href="#" class="btn-clear" style="position: absolute;top: 5px; right: 10px;"><i class="fa fa-times"></i></a>
+      </li>
+      <li class="dropdown-item select-all" choice="select-all">
+        <label>
+          <input type="checkbox" class="select-all-checkbox" /> <span>-- Select All --</span>
+        </label>
+      </li>
+      
       <li class="dropdown-item checkbox" repeat="option" var="option">
         <label var="label">
           <input type="checkbox" var="element" /> <span var="text"></span>
