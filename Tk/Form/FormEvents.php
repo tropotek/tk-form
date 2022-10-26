15fabc4ba11e094306588176b27a9c9eb879136f
tropotek <info@tropotek.com>
2022-10-26 19:56:27 +1000

    ~Auto: Commit

--- Tk/Form/FormEvents.php
+++ Tk/Form/FormEvents.php
@@ -2,9 +2,7 @@
 namespace Tk\Form;
 
 /**
- * @author Michael Mifsud <http://www.tropotek.com/>
- * @see http://www.tropotek.com/
- * @license Copyright 2017 Michael Mifsud
+ * @author Tropotek <http://www.tropotek.com/>
  */
 class FormEvents
 {
@@ -23,16 +21,14 @@ class FormEvents
      * @event \Tk\Event\FormEvent
      */
     const FORM_LOAD_REQUEST = 'form.load.request';
-
-    /**
-     * @event \Tk\Event\FormEvent
-     */
-    const FORM_SHOW = 'form.show';
-
     /**
      * @event \Tk\Event\FormEvent
      */
     const FORM_SUBMIT = 'form.submit';
 
+    /**
+     * @event \Tk\Event\FormEvent
+     */
+    const FORM_SHOW = 'form.show';
 
 }
---
+++ Tk/Form/Renderer.php
@@ -0,0 +1,88 @@
+<?php
+namespace Tk\Form;
+
+use Dom\Template;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Tk\Form;
+use Tk\Traits\SystemTrait;
+
+/**
+ * @author Tropotek <http://www.tropotek.com/>
+ */
+class Renderer
+{
+    use SystemTrait;
+
+    protected Form $form;
+
+    protected ?EventDispatcherInterface $dispatcher;
+
+    protected array $fieldTemplates = [];
+
+    protected array $tabGroupTemplates = [];
+
+    protected array $fieldsetTemplates = [];
+
+
+
+    public function __construct(Form $form)
+    {
+        $this->form = $form;
+        $this->dispatcher = $this->getFactory()->getEventDispatcher();
+        $this->initFieldTemplates($this->getSystem()->makePath($this->getConfig()->get('form.template.path')));
+    }
+
+    protected function initFieldTemplates(string $path): void
+    {
+        // TODO: find out how to get this list dynamically???
+        $tplNames = [
+            'tpl-form',
+            'tpl-hidden',
+            'tpl-none',
+            'tpl-input',
+            'tpl-textarea',
+            'tpl-select',
+            'tpl-checkbox',
+            'tpl-radio',
+            'tpl-switch',
+            'tpl-file',
+            'tpl-button',
+            'tpl-submit',
+            'tpl-link',
+        ];
+        foreach ($tplNames as  $name) {
+            $doc = new \DOMDocument();
+            $doc->loadHTMLFile($path);
+            $tpl = $doc->getElementById($name);
+            $tpl->removeAttribute('id');
+            $this->fieldTemplates[substr($name, 4)] = Template::load($doc->saveXML($tpl));
+        }
+        //vd(array_keys($this->fieldTemplates));
+    }
+
+    public function getFieldTemplate(string $fieldName): Template
+    {
+        return clone ($this->fieldTemplates[$fieldName] ?? $this->fieldTemplates['input']);
+    }
+
+    public function getForm(): Form
+    {
+        return $this->form;
+    }
+
+    public function setForm(Form $form): static
+    {
+        $this->form = $form;
+        return $this;
+    }
+
+    public function getDispatcher(): EventDispatcherInterface
+    {
+        return $this->dispatcher;
+    }
+
+
+
+
+
+}
