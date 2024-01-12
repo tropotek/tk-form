74dd8b63d819fe7237604741a284563a433e89b2
tropotek <info@tropotek.com>
2024-01-12 13:22:59 +1100

    move template paths

--- config/20-config.php
+++ config/20-config.php
@@ -4,9 +4,9 @@ use Tk\Config;
 return function (Config $config)
 {
     // Dom Form renderer
-    $config->set('path.template.form.dom',        '/vendor/ttek/tk-form/templates/Dom/bootstrap5/Form.html');
-    $config->set('path.template.form.dom.inline', '/vendor/ttek/tk-form/templates/Dom/bootstrap5/FormInline.html');
+    $config->set('path.template.form.dom',        '/vendor/ttek/tk-form/templates/dom/bootstrap5/form.html');
+    $config->set('path.template.form.dom.inline', '/vendor/ttek/tk-form/templates/dom/bootstrap5/form_inline.html');
     // Standard Form renderer
-    $config->set('path.template.form.std',        '/vendor/ttek/tk-form/templates/Std/bootstrap5/Form.php');
-    $config->set('path.template.form.std.inline', '/vendor/ttek/tk-form/templates/Std/bootstrap5/FormInline.php');
+    $config->set('path.template.form.std',        '/vendor/ttek/tk-form/templates/std/bootstrap5/form.php');
+    $config->set('path.template.form.std.inline', '/vendor/ttek/tk-form/templates/std/bootstrap5/form_inline.php');
 };
