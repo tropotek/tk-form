39c975d5f7d23a90c80f425f33e3e08b75fd1562
tropotek <info@tropotek.com>
2024-09-14 15:13:00 +1000

    fix single checkbox value

--- Tk/Form/Field/Checkbox.php
+++ Tk/Form/Field/Checkbox.php
@@ -1,7 +1,6 @@
 <?php
 namespace Tk\Form\Field;
 
-use Tk\Db\Mapper\Result;
 use Tk\Form\Field\Option\ArrayIterator;
 
 class Checkbox extends Select
@@ -14,9 +13,9 @@ class Checkbox extends Select
      */
     protected bool $switch = false;
 
-    public function __construct(string $name, null|array|Result|ArrayIterator $optionIterator = null)
+    public function __construct(string $name, null|array|ArrayIterator $optionIterator = null)
     {
-        if (!$optionIterator) $optionIterator = [$name];
+        if (!$optionIterator) $optionIterator = [1];
         parent::__construct($name, $optionIterator);
         if (count($optionIterator) > 1) $this->setMultiple(true);
         $this->setType(self::TYPE_CHECKBOX);
@@ -44,7 +43,7 @@ class Checkbox extends Select
         return $this;
     }
 
-    protected function createIterator(array|Result|ArrayIterator $optionIterator = null, string $nameParam = 'name', string $valueParam = 'id', string $selectAttr = 'checked'): ?Option\ArrayIterator
+    protected function createIterator(array|ArrayIterator $optionIterator = null, string $nameParam = 'name', string $valueParam = 'id', string $selectAttr = 'checked'): ?Option\ArrayIterator
     {
         return parent::createIterator($optionIterator, $nameParam,  $valueParam, $selectAttr);
     }
