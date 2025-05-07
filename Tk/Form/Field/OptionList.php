e47200d96f8ab3303038cd268b6a8fd59d9391b7
tropotek <info@tropotek.com>
2025-05-07 14:26:30 +1000

    ~Auto: Commit

--- Tk/Form/Field/OptionList.php
+++ Tk/Form/Field/OptionList.php
@@ -32,29 +32,55 @@ trait OptionList
         return $this->options;
     }
 
-    public function prependOption(string $name, string $value = '', string $cssClass = ''): static
+    public function prependOption(string $name, ?string $value = null, bool $replace = false): static
     {
-        $opt = new Option($name, $value);
-        if ($cssClass) $opt->addCss($cssClass);
-        return $this->prepend($opt);
+        $opt = new Option($name, $value ?? '');
+        return $this->prepend($opt, $replace);
     }
 
-    public function appendOption(string $name, string $value = '', string $cssClass = ''): static
+    public function prepend(Option $option, bool $replace = false): static
     {
-        $opt = new Option($name, $value);
-        if ($cssClass) $opt->addCss($cssClass);
-        return $this->append($opt);
-    }
-
-    public function append(Option $option): static
-    {
-        $this->options[] = $option;
-        return $this;
-    }
-
-    public function prepend(Option $option): static
-    {
+        if ($replace) {
+            if (empty($option->getValue())) return $this;
+            $idx = $this->getOptionValueIndex($option->getValue());
+            if (!is_null($idx)) {
+                $this->options[$idx] = $option;
+                return $this;
+            }
+        }
         array_unshift($this->options, $option);
+
         return $this;
+    }
+
+    public function appendOption(string $name, ?string $value = null, bool $replace = false): static
+    {
+        $opt = new Option($name, $value ?? '');
+        return $this->append($opt, $replace);
+    }
+
+    public function append(Option $option, bool $replace = false): static
+    {
+        if ($replace) {
+            if (empty($option->getValue())) return $this;
+            $idx = $this->getOptionValueIndex($option->getValue());
+            if (!is_null($idx)) {
+                $this->options[$idx] = $option;
+                return $this;
+            }
+        }
+        $this->options[] = $option;
+
+        return $this;
+    }
+
+    protected function getOptionValueIndex(string $value): ?int
+    {
+        foreach ($this->options as $i => $o) {
+            if ($value === $o->getValue()) {
+                return $i;
+            }
+        }
+        return null;
     }
 }
